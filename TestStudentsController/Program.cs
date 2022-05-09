4e988c7587150a2736dbbc6fb13ff7f402a4b593
ABaldwin1297 <abaldwin1297@gmail.com>
2022-05-09 16:35:36 -0400

    Add project files.

---
+++ TestStudentsController/Program.cs
@@ -0,0 +1,19 @@
+﻿using StudentsController;
+
+
+var studctr1 = new StudentController("localhost\\sqlexpress", "EdDb");
+studctr1.OpenConnection();
+
+var student = new Student() {
+    Firstname = "Andy", Lastname = "Baldwin", StateCode = "OH", GPA = 3.0m,
+    SAT = 1200, MajorId = 1
+};
+
+//var rc = studctr1.AddStudent(student);
+
+//var rc = studctr1.ChangeStudent(student);
+
+student.Id = 1062;
+var rc = studctr1.ChangeStudent(student);
+
+studctr1.CloseConnection();
